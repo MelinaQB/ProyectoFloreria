4d371c847075d658097e9ded1b02d8d297c61ac1
MelinaQB <melina.qberrios00@gmail.com>
2026-06-09 20:25:31 -0400

    Configurar puerto para Render

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,20 +1,21 @@
-import { ValidationPipe } from '@nestjs/common'; // 👈 IMPORTANTE
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // 🔒 ACTIVAMOS EL FILTRO GLOBAL CONTRA INYECCIONES Y DATOS MAL FORMADOS
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, // Borra propiedades que no estén en el DTO
-      forbidNonWhitelisted: false, // Lanza error si mandan campos extraños
-      transform: true, // Convierte los tipos automáticamente
+      whitelist: true,
+      forbidNonWhitelisted: false,
+      transform: true,
     }),
   );
 
-  app.enableCors(); // Mantiene la puerta abierta para que React se conecte
-  await app.listen(3000);
+  app.enableCors();
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
