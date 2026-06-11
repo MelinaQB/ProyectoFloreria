d07046104b3a244b2cb79c3d1136ca8b53ea2cb5
MelinaQB <melina.qberrios00@gmail.com>
2026-06-10 22:31:21 -0400

    debug materiales

--- back/src/ramos/dto/create-ramo.dto.ts
+++ back/src/ramos/dto/create-ramo.dto.ts
@@ -1,11 +1,39 @@
+import { Type } from 'class-transformer';
+import {
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  ValidateNested
+} from 'class-validator';
+
 export class MaterialRecetaDto {
+
+  @Type(() => Number)
+  @IsNumber()
   materialId: number;
+
+  @Type(() => Number)
+  @IsNumber()
   cantidadNecesaria: number;
 }
 
 export class CreateRamoDto {
+
+  @IsString()
+  @IsNotEmpty()
   nombre: string;
+
+  @Type(() => Number)
+  @IsNumber()
   precio: number;
-  adminId: number; // Firma de auditoría
-  materiales: MaterialRecetaDto[]; // Lista de insumos que consumirá el ramo
+
+  @Type(() => Number)
+  @IsNumber()
+  adminId: number;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => MaterialRecetaDto)
+  materiales: MaterialRecetaDto[];
 }
