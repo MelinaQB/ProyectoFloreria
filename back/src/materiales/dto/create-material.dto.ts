f7c731ac6bdf3b5e65076720fcad5e6d6cbf8e2b
MelinaQB <melina.qberrios00@gmail.com>
2026-06-10 22:17:34 -0400

    debug materiales

--- back/src/materiales/dto/create-material.dto.ts
+++ back/src/materiales/dto/create-material.dto.ts
@@ -1,7 +1,25 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+
 export class CreateMaterialDto {
+
+  @IsString()
+  @IsNotEmpty()
   nombre: string;
+
+  @IsString()
+  @IsNotEmpty()
   detalle: string;
+
+  @Type(() => Number)
+  @IsNumber()
   stock: number;
+
+  @Type(() => Number)
+  @IsNumber()
   unidadesPorPaquete: number;
+
+  @Type(() => Number)
+  @IsNumber()
   adminId: number;
 }
